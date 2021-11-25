b04ccb5a6e06771587a2ce03dbc8ff807d814b34
George Maia <67296638+usrmaia@users.noreply.github.com>
2021-11-25 13:36:36 -0300

    3

--- fnc.py
+++ fnc.py
@@ -8,14 +8,14 @@ import string
 # Retorna subfórmula a esquerda de >
 def get_subform_left(A, index):
     open_parenthesis = close_parenthesis = 0
-    index_subform = index
     subform_left = ""
     while close_parenthesis >= open_parenthesis:
-        index_subform -= 1
-        subform_left = A[index_subform] + subform_left
-        if A[index_subform] == "(":
+        index -= 1
+        subform_left = A[index] + subform_left
+
+        if A[index] == "(":
             open_parenthesis += 1
-        elif A[index_subform] == ")":
+        elif A[index] == ")":
             close_parenthesis += 1
     return subform_left[1:]
 
@@ -26,29 +26,47 @@ def get_subform_left(A, index):
 # Retorna subfórmula a direita de >
 def get_subform_right(A, index):
     open_parenthesis = close_parenthesis = 0
-    index_subform = index
     subform_right = ""
     while open_parenthesis >= close_parenthesis:
-        index_subform += 1
-        subform_right = subform_right + A[index_subform]
-        if A[index_subform] == "(":
+        index += 1
+        subform_right = subform_right + A[index]
+
+        if A[index] == "(":
             open_parenthesis += 1
-        elif A[index_subform] == ")":
+        elif A[index] == ")":
             close_parenthesis += 1
     return subform_right[:-1]
+
+# ...-(-(p^q)#(p>q))... |-> pos(#)
+# ...((...)x(...))... |-> pos(x)
+# Retorna operando principal da formula
+def get_key(A, index):
+    open_parenthesis = close_parenthesis = 0
+    while index < len(A):
+        if A[index] == "(":
+            open_parenthesis += 1
+        elif A[index] == ")":
+            close_parenthesis += 1
+
+        if (A[index] == "^" or A[index] == "#" or A[index] == ">"):
+            if open_parenthesis == close_parenthesis + 1:
+                return index
+        index += 1
+    return None
 
 # ...(p>q)
 # ...(p>-q)...
 # ...(-p>q)...
 # ...(-p>-q)...
 # ...((p>q)>p)...
-# Transforma ...((...)>(...))... em ...(-(...)#(...))...
-def get_conditional(A, subform_left, subform_right, index):
-    A_left = A[:index - len(subform_left)] + "-" + subform_left
-    index += 1 
-    A_right = A[index:]
-    return A_left + "#" + A_right 
+# ...((...)>(...))... |-> ...(-(...)#(...))...
+def get_remove_implication(A, subform_left, subform_right, index):
+    A_right = A[index + 1:]
+    A_left = A[:index - len(subform_left)]
+    return A_left + "-" + subform_left + "#" + A_right 
 
+# ...(--(...)>--(...))... |-> ...((...)>(...))...
+# Remove dupla negação
 def remove_double_negation(A):
     A = A.replace("--", "")
     return A
@@ -58,52 +76,41 @@ def remove_double_negation(A):
 # ...(-p>q)...
 # ...(-p>-q)...
 # ...((p>q)>p)...
+# (((...>...)>(...>...))>(...>...)) |-> (-(-(-...#...)#(-...#...))#(-...#...))
 # Remove todas as > da fórmula
-def redefine(A):
+def remove_implication(A):
     index = 0
-    while index != len(A) - 1:
+    while ">" in A:
         index += 1
-        #implication = A[index]
         if(A[index] == ">"):
             subform_left = get_subform_left(A, index)
-            print("subform_left = " + subform_left)
+            #print("subform_left = " + subform_left)
             subform_right = get_subform_right(A, index)
-            print("subform_right = " + subform_right)
-            A = get_conditional(A, subform_left, subform_right, index)
-            A = remove_double_negation(A)
+            #print("subform_right = " + subform_right)
+            A = get_remove_implication(A, subform_left, subform_right, index)
+            index -= 1
+            #A = remove_double_negation(A)
     return A
 
-def get_key(A, index):
-    open_parenthesis = close_parenthesis = 0
-    while index < len(A) - 1:
-        if A[index] == "(":
-            open_parenthesis += 1
-        elif A[index] == ")":
-            close_parenthesis += 1
-
-        if (A[index] == "^" or A[index] == "#"):
-            if open_parenthesis == close_parenthesis + 1:
-                return index
-        index += 1
-    return None
-
-def get_morgan(A, subform_left, subform_right, index):
-    if A[index] == "#":
-        A = A[:index] + "^" + A[index + 1:]
-    elif A[index] == "^":
-        A = A[:index] + "#" + A[index + 1:]
-
-    A = A[:index - len(subform_left) - 2] + A[index - len(subform_left) - 1:]
-
-    index -= 1
-
-    A_right = "-" + A[index + 1:]
-    A_left = A[:index - len(subform_left)] + "-" + subform_left
-    return A_left + A[index] + A_right 
-
-# ...-((...)^(...))... |-> ...(-(...)#-(...))...
 # -(p^q) |-> (-p#-q)
 # -(p#q) |-> (-p^-q)
+# -((...)#(...)) |-> (-(...)^-(...))
+# -((...)^(...)) |-> (-(...)#-(...))
+# Aplica lei de Morgan
+def get_morgan_law(A, subform_left, subform_right, index):
+    if A[index] == "#":
+        key = "^"
+    elif A[index] == "^":
+        key = "#"
+    
+    A_right = A[index + 1:]
+    A_left = A[:index - len(subform_left) - 1 - 1]
+    return A_left + "(" + "-" + subform_left + key + "-" + A_right
+
+# -(p^q) |-> (-p#-q)
+# -(p#q) |-> (-p^-q)
+# -((...#...)#(...#...)) |-> ((-...^-...)^(-...^-...))
+# Aplica Morgan a toda a fórmula
 def morgan_law(A):
     index = 0
     while "-(" in A:
@@ -111,18 +118,16 @@ def morgan_law(A):
             index_key = get_key(A, index + 1)
             subform_left = get_subform_left(A, index_key)
             subform_right = get_subform_right(A, index_key)
-            A = get_morgan(A, subform_left, subform_right, index_key)
+            A = get_morgan_law(A, subform_left, subform_right, index_key)
             A = remove_double_negation(A)
         index += 1
     return A
 
-A = "...-(--(p#q)^-(p^q))..."
-'''
-index_key = get_key(A, 3 + 1)
-subform_left = get_subform_left(A, 11)
-subform_right = get_subform_right(A, 11)
-A = get_morgan(A, subform_left, subform_right, 11)
-A = remove_double_negation(A)
-'''
-A = morgan_law(A)
-print(A)
+def fnc(A):
+    A1 = remove_implication(A)
+    A2 = morgan_law(A1)
+    A3 = remove_double_negation(A2)
+    # Último passo
+    return(A3)
+
+print(fnc(input()))
--- test.py
+++ test.py
@@ -1,18 +1,3 @@
-def get_morgan(A, subform_left, subform_right, index):
-    if A[index] == "#":
-        A = A[:index] + "^" + A[index + 1:]
-    elif A[index] == "^":
-        A = A[:index] + "#" + A[index + 1:]
-
-    A = A[:index - len(subform_left) - 2] + A[index - len(subform_left) - 1:]
-
-    index -= 1
-
-    A_right = "-" + A[index + 1:]
-    A_left = A[:index - len(subform_left)] + "-" + subform_left
-    return A_left + A[index] + A_right 
-
-
-form = "-(p#q)" # (-p^-q)
-form = get_morgan(form, "p", "q", 3)
-print(form)
+A = "-(aaaaaa"
+if "-(" in A[1:1 + 2]:
+    print("ok")
